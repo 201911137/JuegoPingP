80ddc4cd522dceb8fa25f729367249aa37a2f3e5
marlpaz <marlpaz@VNS-B8QY7S2>
2020-06-29 21:46:59 -0600

    Commint 2

---
+++ Juego_ping_pong/Juego.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Juego_ping_pong
+{
+    partial class Juego
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Juego));
+            this.pbJugador1 = new System.Windows.Forms.PictureBox();
+            this.pbJugador2 = new System.Windows.Forms.PictureBox();
+            this.pbPantallaPequeña = new System.Windows.Forms.PictureBox();
+            this.tmrTiempoActualizar = new System.Windows.Forms.Timer(this.components);
+            this.tmrTiempoDibujar = new System.Windows.Forms.Timer(this.components);
+            this.pbBola = new System.Windows.Forms.PictureBox();
+            this.lblScore2 = new System.Windows.Forms.Label();
+            this.lblScore1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbJugador1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbJugador2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPantallaPequeña)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBola)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbJugador1
+            // 
+            this.pbJugador1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pbJugador1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbJugador1.Location = new System.Drawing.Point(24, 111);
+            this.pbJugador1.Name = "pbJugador1";
+            this.pbJugador1.Size = new System.Drawing.Size(70, 126);
+            this.pbJugador1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbJugador1.TabIndex = 2;
+            this.pbJugador1.TabStop = false;
+            this.pbJugador1.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pbJugador2
+            // 
+            this.pbJugador2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pbJugador2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbJugador2.Location = new System.Drawing.Point(940, 111);
+            this.pbJugador2.Name = "pbJugador2";
+            this.pbJugador2.Size = new System.Drawing.Size(70, 126);
+            this.pbJugador2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbJugador2.TabIndex = 1;
+            this.pbJugador2.TabStop = false;
+            // 
+            // pbPantallaPequeña
+            // 
+            this.pbPantallaPequeña.Location = new System.Drawing.Point(24, 12);
+            this.pbPantallaPequeña.Name = "pbPantallaPequeña";
+            this.pbPantallaPequeña.Size = new System.Drawing.Size(100, 50);
+            this.pbPantallaPequeña.TabIndex = 4;
+            this.pbPantallaPequeña.TabStop = false;
+            // 
+            // tmrTiempoActualizar
+            // 
+            this.tmrTiempoActualizar.Interval = 16;
+            this.tmrTiempoActualizar.Tick += new System.EventHandler(this.tmrTiempoActualizar_Tick);
+            // 
+            // tmrTiempoDibujar
+            // 
+            this.tmrTiempoDibujar.Interval = 16;
+            this.tmrTiempoDibujar.Tick += new System.EventHandler(this.tmrTiempoDibujar_Tick);
+            // 
+            // pbBola
+            // 
+            this.pbBola.Image = ((System.Drawing.Image)(resources.GetObject("pbBola.Image")));
+            this.pbBola.Location = new System.Drawing.Point(411, 216);
+            this.pbBola.Name = "pbBola";
+            this.pbBola.Size = new System.Drawing.Size(39, 34);
+            this.pbBola.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbBola.TabIndex = 5;
+            this.pbBola.TabStop = false;
+            // 
+            // lblScore2
+            // 
+            this.lblScore2.AutoSize = true;
+            this.lblScore2.Font = new System.Drawing.Font("Microsoft Sans Serif", 120F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore2.ForeColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.lblScore2.Location = new System.Drawing.Point(469, 12);
+            this.lblScore2.Name = "lblScore2";
+            this.lblScore2.Size = new System.Drawing.Size(166, 181);
+            this.lblScore2.TabIndex = 10;
+            this.lblScore2.Text = "0";
+            // 
+            // lblScore1
+            // 
+            this.lblScore1.AutoSize = true;
+            this.lblScore1.Font = new System.Drawing.Font("Microsoft Sans Serif", 120F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore1.ForeColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.lblScore1.Location = new System.Drawing.Point(214, 9);
+            this.lblScore1.Name = "lblScore1";
+            this.lblScore1.Size = new System.Drawing.Size(166, 181);
+            this.lblScore1.TabIndex = 9;
+            this.lblScore1.Text = "0";
+            // 
+            // Juego
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(1012, 733);
+            this.Controls.Add(this.lblScore2);
+            this.Controls.Add(this.lblScore1);
+            this.Controls.Add(this.pbBola);
+            this.Controls.Add(this.pbPantallaPequeña);
+            this.Controls.Add(this.pbJugador1);
+            this.Controls.Add(this.pbJugador2);
+            this.Name = "Juego";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Juego";
+            this.Load += new System.EventHandler(this.Juego_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbJugador1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbJugador2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPantallaPequeña)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBola)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        //private System.Windows.Forms.PictureBox Imgfondo2;
+        private System.Windows.Forms.PictureBox pbJugador2;
+        private System.Windows.Forms.PictureBox pbJugador1;
+        private System.Windows.Forms.PictureBox pbPantallaPequeña;
+        private System.Windows.Forms.Timer tmrTiempoActualizar;
+        private System.Windows.Forms.Timer tmrTiempoDibujar;
+        private System.Windows.Forms.PictureBox pbBola;
+        private System.Windows.Forms.Label lblScore2;
+        private System.Windows.Forms.Label lblScore1;
+    }
+}
---
+++ Juego_ping_pong/Juego.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace Juego_ping_pong
+{
+    public partial class Juego : Form
+    {   //Parametros del formulario
+        private const int AnchoPantalla = 1028;
+        private const int AlturaPantalla = 768;
+        //Velocidad base y Maxima
+        private const int BaseVelPelota = 2;
+        private int Nivel = 7;
+        //Elementos para Jugar
+        private PongOrigen Jugador1;
+        private PongOrigen Jugador2;
+        private PongBola Bola;
+        //Pantalla del juego y variable aleatoria para movimiento de pelota
+        private PongCubo PantallaPequeña;
+        private Random random;
+        //Puntaje de los jugadores
+        private int PuntuacionJ1;
+        private int PuntuacionJ2;
+        public Juego()
+        {
+            InitializeComponent();
+            ClientSize = new Size(AnchoPantalla, AlturaPantalla);
+            //Initialize();
+            Load += Juego_Load;
+            pbJugador2.BackColor = Color.Transparent;
+            pbJugador1.BackColor = Color.Transparent;
+            pbBola.BackColor = Color.Transparent;
+        }
+        #region Metodos del juego
+
+        private void ComenzarJuego()
+        {
+            _isGameRunning = true;
+            ResetearBola();
+            pbPantallaPequeña.Hide();
+
+        }
+        #endregion
+        #region Eventos
+        private void Juego_Load(object sender, EventArgs e)
+        {
+            CargarGraficos();
+        }
+        private void tmrTiempoActualizar_Tick(object sender, EventArgs e)
+        {
+            ActualizarEscena();
+        }
+        private void tmrTiempoDibujar_Tick(object sender, EventArgs e)
+        {
+            DibujarEscena();
+        }
+        #endregion
+        #region EngineMethods
+        private void Initialize()
+        {
+            random = new Random();
+            Jugador1 = new PongOrigen();
+            Jugador2 = new PongOrigen
+            {
+                Posicion = new Point(AnchoPantalla - 3, AlturaPantalla / 2)
+            };
+            Bola = new PongBola
+            {
+                Velocidad = new Point(2, 5)
+            };
+            PantallaPequeña = new PongCubo();
+            PantallaPequeña.Posicion = new Point(0, 0);
+            PantallaPequeña.Ancho = AnchoPantalla;
+            PantallaPequeña.Altura = AlturaPantalla;
+        }
+        private void CargarGraficos()
+        {
+            pbJugador1.Load("RacketLeft.png");
+            Jugador1.Texturas = pbJugador1;
+
+            pbJugador2.Load("RacketRigth.png");
+            Jugador2.Texturas = pbJugador2;
+
+            pbBola.Load("Bola.png");
+            Bola.Texturas = pbBola;
+
+            pbPantallaPequeña.Load("Fondo.png");
+            PantallaPequeña.Texturas = pbPantallaPequeña;
+        }
+        private void ActualizarEscena()
+        {
+            if (_isGameRunning)
+            {
+                ActualizarJugador();
+                Bola.Actualizar();
+
+                CheckColisionMuro();
+                CheckMuroAfuera();
+                CheckColisionRaqueta();
+            }
+            else if (MouseButtons == MouseButtons.Left)
+            {
+                ComenzarJuego();
+            }
+        }
+        private bool _isGameRunning;
+        private void DibujarEscena()
+        {
+            if (_isGameRunning)
+            {
+                Jugador1.Dibujar();
+                Jugador2.Dibujar();
+                Bola.Dibujar();
+            }
+            else
+            {
+                PantallaPequeña.Dibujar();
+            }
+        }
+        #endregion
+        #region Mechanics
+        private int ActualY;
+        private void ActualizarJugador()
+        {
+            int JugadorX = 0 + 30;
+            int playerY = PointToClient(MousePosition).Y;
+            Jugador1.Posicion = new Point(JugadorX, playerY);
+
+            if (Jugador1.Texturas.Bottom >= AlturaPantalla)
+            {
+                Jugador1.Posicion = new Point(JugadorX, AlturaPantalla - Jugador1.Origen.Y - 1);
+            }
+            else if (Jugador1.Texturas.Top <= 0)
+            {
+                Jugador1.Posicion = new Point(JugadorX, Jugador1.Origen.Y + 1);
+            }
+            if (Keyboard.IsKeyDown(Key.S))
+            {
+                if (Jugador2.Texturas.Bottom >= AlturaPantalla)
+                {
+                    ActualY -= 0;
+                }
+                else
+                {
+                    ActualY += 30;
+                }
+                Jugador2.Posicion = new Point(AnchoPantalla - 30, ActualY);
+            }
+            else if (Keyboard.IsKeyDown(Key.W))
+            {
+                if (Jugador2.Texturas.Top <= 0)
+                {
+                    ActualY += 0;
+                }
+                else
+                {
+                    ActualY -= 30;
+                }
+                int Jugador2X = AnchoPantalla - 30;
+                int Jugador2Y = ActualY;
+                Jugador2.Posicion = new Point(Jugador2X, Jugador2Y);
+            }
+        }
+        private void ResetearBola()
+        {
+            Nivel = 7;
+            int VelocidadY = GenerateBallY();
+            int VelocidadX = GenerarBolaX();
+            Bola.Posicion = new Point(AnchoPantalla / 2, AlturaPantalla / 2);
+            Bola.Velocidad = new Point(VelocidadX, VelocidadY);
+
+            ActualBolaX = VelocidadX;
+        }
+        private int GenerarBolaX()
+        {
+            Nivel += 1;
+            int velocityX = Nivel;
+            if (random.Next(2) == 0)
+            {
+                velocityX *= -1;
+            }
+            return velocityX;
+        }
+        private int GenerateBallY()
+        {
+            Nivel += (int).5;
+            int velocityY = random.Next(0, Nivel);
+            if (random.Next(2) == 0)
+            {
+                velocityY *= -1;
+            }
+            return velocityY;
+        }
+        #endregion
+        #region Collision
+        private int ActualBolaX;
+        private void CheckColisionMuro()
+        {
+            if (pbBola.Bottom >= AlturaPantalla)
+            {
+                Bola.Velocidad = new Point(ActualBolaX, -BaseVelPelota);
+            }
+            else if (pbBola.Top <= 0)
+            {
+                Bola.Velocidad = new Point(ActualBolaX, BaseVelPelota);
+            }
+        }
+        private void CheckMuroAfuera()
+        {
+            if (pbBola.Left < 0)
+            {
+                ResetearBola();
+                PuntuacionJ2 += 1;
+                lblScore2.Text = PuntuacionJ2.ToString();
+            }
+            else if (pbBola.Right > AnchoPantalla)
+            {
+                ResetearBola();
+                PuntuacionJ1 += 1;
+                lblScore1.Text = PuntuacionJ1.ToString();
+            }
+        }
+        private void CheckColisionRaqueta()
+        {
+            if (Bola.EsquIzquierda.X < Jugador1.EsqDerecha.X &&
+                Bola.EsqInferiorIzquierda.Y > Jugador1.EsqInferiorDerecha.Y &&
+                Bola.EsqDerecha.Y < Jugador1.EsqInferiorDerecha.Y)
+            {
+                ActualBolaX = GenerarBolaX();
+                if (ActualBolaX < 0)
+                {
+                    ActualBolaX *= -1;
+                }
+                Bola.Velocidad = new Point(ActualBolaX, GenerateBallY());
+            }
+            if (Bola.EsqDerecha.X > Jugador2.EsquIzquierda.X &&
+                Bola.EsqInferiorDerecha.Y > Jugador2.EsquIzquierda.Y &&
+                Bola.EsqDerecha.Y < Jugador2.EsqInferiorIzquierda.Y)
+            {
+                ActualBolaX = GenerarBolaX();
+                if (ActualBolaX > 0)
+                {
+                    ActualBolaX *= -1;
+                }
+                Bola.Velocidad = new Point(ActualBolaX, GenerateBallY());
+            }
+        }
+        #endregion
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
+
